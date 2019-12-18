3db4b3855cbfc8fc775691e3eb4f79967401ec13
Your Name <you@example.com>
2019-12-18 22:44:11 +0800

    an

--- src/instance.ts
+++ src/instance.ts
@@ -4,9 +4,9 @@ import Sampler from './Components/Instrument/Sampler'
 import MoaSynth from './Components/Instrument/MoaSynth'
 import Module from './Components/Module'
 import MoaReverb from  './Components/Effect/MoaReverb'
+import { loaders } from 'pixi.js'
 
 function initStage() {
-
     let sampler = new Sampler();
     sampler.container.position.set(400, 300);
 
@@ -22,7 +22,16 @@ function initStage() {
     let reverb = new MoaReverb();
     reverb.container.position.set(700, 350);
 
+    let loading = document.querySelector('.loading') as HTMLDivElement;
+    loading.innerHTML = 'Loading audio resources...'
+    loading.style.opacity = '0.8';
+
     sampler.readyHook = () => {
+        loading.style.opacity = '0';
+        setTimeout(() => {
+            loading.style.display = 'none';
+        }, 500);
+
         Module.manualConnect(keyboard, moaSynth);
         Module.manualConnect(moaSynth, reverb);
         Module.manualConnect(reverb, master);
